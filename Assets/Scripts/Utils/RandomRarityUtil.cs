57832fe3cf07c7adfe14c93565ff5653451d846b
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2024-11-01 02:12:37 -0400

    fix: Change mono effect to exclude self

--- Assets/Scripts/Utils/RandomRarityUtil.cs
+++ Assets/Scripts/Utils/RandomRarityUtil.cs
@@ -1,9 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class RandomRarityUtil 
+public class RandomRarityUtil
 {
     private static readonly System.Random _random = new();
 
